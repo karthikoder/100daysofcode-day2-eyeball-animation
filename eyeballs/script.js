bb9fb328802b343d0294a88bb7671fa9ec8bf254
karthik enumarthi <kartheekenumarthi@gmail.com>
2019-11-12 09:28:43 +0530

    Delete script.js

--- eyeballs/script.js
+++
@@ -1,8 +0,0 @@
-var balls = document.getElementsByClassName("eyeball");
-   function fun(event)
-   {
-    var x = event.clientX * 100 / window.innerWidth + "%";
-    var y = event.clientY * 100 / window.innerHeight + "%";
-  document.getElementById("demo").style.top =y;
-  document.getElementById("demo").style.left =x;
-   }
